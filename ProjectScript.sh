5b2d5e8f5b4a16a42e3b6407c4c62552d3b06267
Loan Duong <lduong@crcfe01.crc.nd.edu>
2021-10-15 14:25:19 -0400

    project script, added code to return text files with proteome # and matches for each gene

--- ProjectScript.sh
+++ ProjectScript.sh
@@ -17,11 +17,43 @@ cd proteomes
 for proteome in *.fasta
 do
 ../hmmsearch ../BUILD_hsp70.hmm $proteome >../SEARCH_proteomes/hsp70/$proteome.out
-echo "$proteome searched for hsp70"
 done
+echo "all proteomes searched for hsp70"
+
 for proteome in *.fasta
 do
 ../hmmsearch ../BUILD_mcrA.hmm $proteome >../SEARCH_proteomes/mcrA/$proteome.out
-echo "$proteome searched for mcrA"
 done
+echo "all proteomes searched for mcrA"
+
+#search through previous resultant files for the number of matches that passed the fwd filter
+cd ../SEARCH_proteomes/hsp70
+
+for searchedproteome in *.out
+do
+cat $searchedproteome | grep -E "target sequence database">>../tempMATCHES_hsp70.txt
+cat $searchedproteome | grep -E "Passed Fwd filter:" >>../tempMATCHES_hsp70.txt
+done
+cd ..
+grep -E -o "proteome_[0-9]*" tempMATCHES_hsp70.txt > temp_hsp70_proteomenumbers
+grep -E -o " [0-9] " tempMATCHES_hsp70.txt > temp_hsp70_matchnumbers
+paste temp_hsp70_proteomenumbers temp_hsp70_matchnumbers | column -s $'\t' -t > MATCHES_hsp70.txt
+rm temp*
+echo "See matches in proteomes for hsp70 in SEARCH_proteomes/MATCHES_hsp70.txt"
+
+cd mcrA
+
+for searchedproteome in *.out
+do
+cat $searchedproteome | grep -E "target sequence database">>../tempMATCHES_mcrA.txt
+cat $searchedproteome | grep -E "Passed Fwd filter:" >>../tempMATCHES_mcrA.txt
+done
+cd ..
+grep -E -o "proteome_[0-9]*" tempMATCHES_mcrA.txt > temp_mcrA_proteomenumbers
+grep -E -o " [0-9] " tempMATCHES_mcrA.txt > temp_mcrA_matchnumbers
+paste temp_mcrA_proteomenumbers temp_mcrA_matchnumbers | column -s $'\t' -t > MATCHES_mcrA.txt
+rm temp*
+
+echo "See matches in proteomes for mcrA in SEARCH_proteomes/MATCHES_mcrA.txt"
+
 
